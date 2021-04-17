50652a97ae57ac484378c23499c6cc66057ddc31
Alexander <aleksgmr@gmail.com>
2021-04-17 19:56:04 +0300

    API setup in app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from flask_restful import Api
+from data import db_session, anecdotes_resource
+
 
 app = Flask(__name__)
+api = Api(app)
 
+api.add_resource(anecdotes_resource.AnecdotesResource, "/anecdote")
+api.add_resource(anecdotes_resource.AnecdotesListResource, "/anecdotes/page")
+api.add_resource(anecdotes_resource.AnecdotesTopResource, "/anecdotes/top")
+
+if __name__ == '__main__':
+    db_session.global_init('db/anecdotes.db')
+    app.run(port=8080, host='0.0.0.0')
