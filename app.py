1a1f40e763ccb0c0d76552e10d2cb24c3570388e
Alexander <aleksgmr@gmail.com>
2021-04-17 21:20:57 +0300

    fix log +++++

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, render_template
 from flask_login import LoginManager, login_user
-from data.db_session import *
+from data import *
 from data.users import User
 from forms.login_form import LoginForm
 from forms.register_form import RegisterForm
-from data import anecdotes_resource
+from data import anecdotes_resource, db_session
 from flask_restful import Api
 
 
@@ -16,7 +16,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    db_sess = create_session()
+    db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
 
@@ -25,7 +25,7 @@ def login():
     form = LoginForm()
     context = {'form': form, 'title': 'Авторизация'}
     if form.validate_on_submit():
-        db_sess = create_session()
+        db_sess = db_session.create_session()
         user = db_sess.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
@@ -39,7 +39,7 @@ def register():
     form = RegisterForm()
     context = {'form': form, 'title': 'Регистрация'}
     if form.validate_on_submit():
-        db_sess = create_session()
+        db_sess = db_session.create_session()
         if form.repeat_password.data != form.hashed_password.data:
             return render_template('register.html', message='Пароли не совпадают', **context)
         if form.email.data in [user.email for user in db_sess.query(User).all()]:
