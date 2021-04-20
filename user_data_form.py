8f78b4c53eb4da1ad977e6b52088bbd0a27500cc
artemPersik <artemstavropol2003@gmail.com>
2021-04-20 17:59:07 +0300

    add user page

---
+++ user_data_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+
+class UserData(FlaskForm):
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Мыло', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    avatar = FileField('image', validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Добавить')
