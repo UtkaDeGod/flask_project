8cea4d15087b6f8790414ce512fea7c8f3619a7b
Real Keks <gdjvvjfisnkd@gmail.com>
2021-05-18 17:45:34 +0300

    Fix bug with mysql

--- app_heroku.py
+++ app_heroku.py
@@ -11,7 +11,7 @@ def start_app(secret_key):
     password = os.environ.get('PASSWORD')
     db_host = os.environ.get('DB_HOST')
     db_port = os.environ.get('DB_PORT')
-    db_name = os.environ.get('DB_PORT')
+    db_name = os.environ.get('DB_NAME')
 
     global_init('mysql', create_conn_args_string(login, password, db_host, db_port, db_name))
     app = create_app(secret_key)
